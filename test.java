79bb4d4952fbef3a4dbd6450cde57afaf417bb8f
Young <485416879@qq.com>
2022-12-26 18:37:36 +0800

    远程库冲突演示

--- test.java
+++ test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("Hello World!master");
         System.out.println("冲突合并演示 master");
         System.out.println("冲突合并演示 hot-fix");
+        System.out.println("远程库冲突演示 本地");
     }
 }
